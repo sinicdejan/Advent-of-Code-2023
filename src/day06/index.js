d6a8f315b3b47e1e93bf9d1dcf92de097f38f9c9
dejansinic <dejan.sinic@agilcon.com>
2023-12-06 09:35:22 +0100

    day 6

---
+++ src/day06/index.js
@@ -0,0 +1,66 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput) => rawInput;
+
+const part1 = (rawInput) => {
+  const input = rawInput;
+
+  let times = input.split('\n')[0].split(/\s+/).splice(1).map(Number);
+  let distances = input.split('\n')[1].split(/\s+/).splice(1).map(Number);
+
+  let result = 1;
+
+  for(let i = 0; i < times.length; i++) {
+    let wins = 0;
+    for(let j = 0; j < times[i]; j++) {
+      if(((times[i] - j) * j) > distances[i]) {
+        wins++;
+      }
+    }
+    result *= wins;
+  }
+
+  return result;
+};
+
+const part2 = (rawInput) => {
+  const input = rawInput;
+
+  let times = parseInt(input.split('\n')[0].split(':')[1].replace(/\s/g,''));
+  let distances = parseInt(input.split('\n')[1].split(':')[1].replace(/\s/g,''));
+
+  let wins = 0;
+
+  for(let waitingTime = 0; waitingTime < times; waitingTime++) {
+    if(((times - waitingTime) * waitingTime) > distances) {
+      wins++;
+    }
+  }
+
+  return wins;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `Time:      7  15   30
+Distance:  9  40  200`,
+        expected: 288,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `Time:      7  15   30
+Distance:  9  40  200`,
+        expected: 71503,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
